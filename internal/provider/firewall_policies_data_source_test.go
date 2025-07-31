e6fb34e8c8b633e02011c2dd0222852abef6f910
mromosan <mromosan@arsys.es>
2025-07-31 10:17:24 +0200

    feat: ARSYSLAB-206: Firewall Policies tests for resource and datasource

---
+++ internal/provider/firewall_policies_data_source_test.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"testing"
+	"time"
+)
+
+func TestAccFirewallPoliciesDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			TestAccPreCheck(t)
+			time.Sleep(5 * time.Second)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_0_0),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFirewallPoliciesDataSourceConfig(),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.arsys-baremetal_firewall_policies.all",
+						tfjsonpath.New("firewall_policies"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.arsys-baremetal_firewall_policies.all",
+						tfjsonpath.New("id"),
+						knownvalue.NotNull(),
+					),
+				},
+			},
+		},
+	})
+}
+
+func testAccFirewallPoliciesDataSourceConfig() string {
+	return `
+data "arsys-baremetal_firewall_policies" "all" {}
+`
+}
---
+++ internal/provider/firewall_policy_resource_test.go
@@ -0,0 +1,107 @@
+package provider
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"regexp"
+	"terraform-provider-arsys-baremetal/internal/util"
+	"testing"
+	"time"
+)
+
+func TestAccFirewallPolicyResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			TestAccPreCheck(t)
+			time.Sleep(5 * time.Second)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_0_0),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testFirewallPolicyWithRulesConfig,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("id"),
+						knownvalue.StringRegexp(regexp.MustCompile(util.HexID32Pattern)),
+					),
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact("firewall-policy-with-rules"),
+					),
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("state"),
+						knownvalue.StringExact("ACTIVE"),
+					),
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("rules"),
+						knownvalue.SetSizeExact(2),
+					),
+				},
+			},
+			{
+				ResourceName:      "arsys-baremetal_firewall_policy.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccFirewallPolicyResourceConfig(
+					"firewall-policy-updated",
+					"Updated description",
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact("firewall-policy-updated"),
+					),
+					statecheck.ExpectKnownValue(
+						"arsys-baremetal_firewall_policy.test",
+						tfjsonpath.New("description"),
+						knownvalue.StringExact("Updated description"),
+					),
+				},
+			},
+		},
+	})
+}
+
+func testAccFirewallPolicyResourceConfig(name, description string) string {
+	return fmt.Sprintf(`
+resource "arsys-baremetal_firewall_policy" "test" {
+  name        = "%s"
+  description = "%s"
+}
+`, name, description)
+}
+
+var testFirewallPolicyWithRulesConfig = `
+resource "arsys-baremetal_firewall_policy" "test" {
+  name        = "firewall-policy-with-rules"
+  description = "Firewall policy with rules for testing"
+  rules = [
+    {
+      protocol  = "TCP"
+      port_from = 22
+      port_to   = 22
+      source    = "192.168.1.0/24"
+    },
+    {
+      protocol  = "TCP"
+      port_from = 80
+      port_to   = 80
+      source    = "0.0.0.0"
+    }
+  ]
+}
+`
