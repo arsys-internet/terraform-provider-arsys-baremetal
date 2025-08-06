c4283ccf0738c953c01168488ad100eb90b95de3
iramirezsuarez <iramirez@arsys.es>
2025-08-06 15:24:59 +0200

    feat: ARSYSLAB-211: fix PR 1

--- examples/data-sources/serverAppliance/main.tf
+++ examples/data-sources/serverAppliance/main.tf
@@ -1,7 +1,7 @@
-# Example of using the public IP data source in the Bare Metal Cloud provider.
+# Example of using the server appliance data source in the Bare Metal Cloud provider.
 data "arsys-baremetal_server_appliances" "all" {}
 
-# Example of using the public IP data source in the Bare Metal Cloud provider with a specific ID.
+# Example of using the server appliance data source in the Bare Metal Cloud provider with a specific ID.
 data "arsys-baremetal_server_appliance" "example" {
   id = var.server_appliance_id
 }
