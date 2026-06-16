ea4635f65e076ee6bf71f8135810d8d16f840790
mromosan <mromosan@arsys.es>
2026-06-16 15:04:31 +0200

    chore: remove goreleaser boilerplate comment in main.go

--- main.go
+++ main.go
@@ -10,14 +10,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
-	version string = "dev"
-
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
-)
+// version is set by goreleaser at build time.
+var version string = "dev"
 
 func main() {
 	var debug bool
